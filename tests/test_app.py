dc81847fe6c6e0fd8e3d6d8ec480d69814b767c5
Saddff <133538823+Saddff2@users.noreply.github.com>
2024-05-28 22:54:03 +0300

    Delete directory

--- tests/test_app.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-def test_home():
-    with app.test_client() as client:
-        response = client.get('/')
-        assert response.status_code == 200
-        assert response.data.decode() == "Hello from Daniel Tsoref"
